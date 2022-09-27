896bdd5996a625408147504edee9cabf91c31eb0
Vosol Vojtěch <Vosol.Vo.2022@skola.ssps.cz>
2022-09-27 11:28:58 +0200

    first commit

---
+++ kalkulacka/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulacka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo = int.Parse(Console.ReadLine());
+            int cislo1 = int.Parse(Console.ReadLine());
+            string operace = Console.ReadLine();
+            double vysledek = 0;
+
+            if (operace == "+")
+            {
+                vysledek = cislo + cislo1;
+            }
+            else if (operace == "*")
+            {
+                vysledek = cislo * cislo1;
+            }
+            else if (operace == "/")
+            {
+                vysledek = cislo / cislo1;
+            }
+            else if (operace == "-")
+            {
+                vysledek = cislo - cislo1;
+            }
+            else if (operace == "%")
+            {
+                vysledek = cislo % cislo1;
+            }
+            else 
+            {
+                Console.WriteLine("Wrong answer");
+            }
+
+            Console.WriteLine("Vysledek: " + vysledek);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kalkulacka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("kalkulacka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kalkulacka")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("aa3603a6-8b25-45d3-a524-472bba2ca6ec")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
